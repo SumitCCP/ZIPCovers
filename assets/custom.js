97c7ec45797d9a6efe93516de0140adf5b0f8197
SumitCCP <sumit@cozycornerpatios.com>
2025-12-08 22:43:36 +0530

    first commit

---
+++ assets/custom.js
@@ -0,0 +1,27 @@
+// function openTab(e, a) {
+//   var i, n, t;
+//   for (i = 0, n = document.getElementsByClassName("tabcontent-cus"); i < n.length; i++) {
+//     n[i].style.display = "none";
+//   }
+//   for (i = 0, t = document.getElementsByClassName("tablinks-cus"); i < t.length; i++) {
+//     t[i].className = t[i].className.replace(" active", "");
+//   }
+//   document.getElementById(a).style.display = "block";
+//   e.currentTarget.className += " active";
+// }
+
+// $(document).ready(function () {
+//   $(".thecard, .thecard1").mouseenter(function () {
+//     $(this).addClass("thecard51");
+//   }).mouseleave(function () {
+//     $(this).removeClass("thecard51");
+//   });
+
+//   const cusTab = document.getElementById("tab-cus-active");
+//   if (cusTab) cusTab.click();
+
+//   // Show all items without pagination
+//   $("#all_tags").children().css("display", "block");
+//   $("#residential_tag").children().css("display", "block");
+//   $("#commercial_tag").children().css("display", "block");
+// });
---
+++ assets/lh-6682ef0be1e2eefc3101fa12.js
@@ -0,0 +1,35 @@
+;(function($){var ajax = (p, c, e) => { $.ajax({url: window.location.href, method: "POST",							data: $.extend(true, p !== undefined && typeof p == "object" ? p : {}, {								action: "layouthub_section_ajax", section_id: "09k3cs2nn5jv"							}), success: c, error: e});						}, cb  = function(section, $) {let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 10000); // Change image every 10 seconds
+}
+
+// for scndry slide 
+let newslideIndex = 0;
+shownewSlides();
+
+function shownewSlides() {
+
+    let i;
+    let slides = document.getElementsByClassName("mynewSlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    newslideIndex++;
+    if (newslideIndex > slides.length) {
+        newslideIndex = 1
+    }
+    slides[newslideIndex - 1].style.display = "block";
+    setTimeout(shownewSlides, 10000); // Change image every 10 seconds
+};};					cb.bind($('section[data-section-id="09k3cs2nn5jv"]').get(0))({						url: "https://library.layouthub.com/HUB/files/TWFyLS0yMDIx/NjE1Mjc3MDI3/release-3-2021/pages/YoXW4G32DBhE08fl/top/",						settings: {}					}, jQuery);})(jQuery);;(function($){var ajax = (p, c, e) => { $.ajax({url: window.location.href, method: "POST",							data: $.extend(true, p !== undefined && typeof p == "object" ? p : {}, {								action: "layouthub_section_ajax", section_id: "j8xplp3cvjq"							}), success: c, error: e});						}, cb  = function(section, $) {};					cb.bind($('section[data-section-id="j8xplp3cvjq"]').get(0))({						url: "https://library.layouthub.com/HUB/files/TWFyLS0yMDIx/NjE1Mjc3MDI3/release-3-2021/pages/YoXW4G32DBhE08fl/top/",						settings: {}					}, jQuery);})(jQuery);;(function($){var ajax = (p, c, e) => { $.ajax({url: window.location.href, method: "POST",							data: $.extend(true, p !== undefined && typeof p == "object" ? p : {}, {								action: "layouthub_section_ajax", section_id: "6rqqyl64wei"							}), success: c, error: e});						}, cb  = function(section, $) {};					cb.bind($('section[data-section-id="6rqqyl64wei"]').get(0))({						url: "https://library.layouthub.com/HUB/files/RmViLS0yMDIx/NjEzNzA2NjU3/release-2-2021/pages/ZmHEruNeFAW5Gfd8/call-to-action/",						settings: {}					}, jQuery);})(jQuery);;(function($){var ajax = (p, c, e) => { $.ajax({url: window.location.href, method: "POST",							data: $.extend(true, p !== undefined && typeof p == "object" ? p : {}, {								action: "layouthub_section_ajax", section_id: "ff3orq6ftlt"							}), success: c, error: e});						}, cb  = function(section, $) {};					cb.bind($('section[data-section-id="ff3orq6ftlt"]').get(0))({						url: "https://library.layouthub.com/HUB/files/TWFyLS0yMDIx/NjE1Mjc3MDI3/release-3-2021/pages/YoXW4G32DBhE08fl/top/",						settings: {}					}, jQuery);})(jQuery);;(function($){var ajax = (p, c, e) => { $.ajax({url: window.location.href, method: "POST",							data: $.extend(true, p !== undefined && typeof p == "object" ? p : {}, {								action: "layouthub_section_ajax", section_id: "5d0u8lewp55"							}), success: c, error: e});						}, cb  = function(section, $) {};					cb.bind($('section[data-section-id="5d0u8lewp55"]').get(0))({						url: "https://library.layouthub.com/HUB/files/TWFyLS0yMDIx/NjE1Mjc3MDI3/release-3-2021/pages/YoXW4G32DBhE08fl/top/",						settings: {}					}, jQuery);})(jQuery);;(function($){var ajax = (p, c, e) => { $.ajax({url: window.location.href, method: "POST",							data: $.extend(true, p !== undefined && typeof p == "object" ? p : {}, {								action: "layouthub_section_ajax", section_id: "qfmwzs6cw6r"							}), success: c, error: e});						}, cb  = function(section, $) {};					cb.bind($('section[data-section-id="qfmwzs6cw6r"]').get(0))({						url: "https://library.layouthub.com/HUB/files/TWFyLS0yMDIx/NjE1Mjc3MDI3/release-3-2021/pages/YoXW4G32DBhE08fl/top/",						settings: {}					}, jQuery);})(jQuery);;(function($){var ajax = (p, c, e) => { $.ajax({url: window.location.href, method: "POST",							data: $.extend(true, p !== undefined && typeof p == "object" ? p : {}, {								action: "layouthub_section_ajax", section_id: "4p6zq72dw2u"							}), success: c, error: e});						}, cb  = function(section, $) {};					cb.bind($('section[data-section-id="4p6zq72dw2u"]').get(0))({						url: "https://library.layouthub.com/HUB/files/Tm92LS0yMDE5/NTc0ODY0ODc5/First_release/pages/HomePage Gime/collection/",						settings: {}					}, jQuery);})(jQuery);;(function($){var ajax = (p, c, e) => { $.ajax({url: window.location.href, method: "POST",							data: $.extend(true, p !== undefined && typeof p == "object" ? p : {}, {								action: "layouthub_section_ajax", section_id: "8k92yftm115"							}), success: c, error: e});						}, cb  = function(section, $) {};					cb.bind($('section[data-section-id="8k92yftm115"]').get(0))({						url: "https://library.layouthub.com/HUB/files/TWFyLS0yMDIx/NjE1Mjc3MDI3/release-3-2021/pages/YoXW4G32DBhE08fl/top/",						settings: {}					}, jQuery);})(jQuery);;console.log('This page layout has been built by https://www.layouthub.com');
